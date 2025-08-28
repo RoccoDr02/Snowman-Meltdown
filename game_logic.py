8548b1b32cc0207b7370447cc09d6c2b0d15ca52
RoccoDr02 <roccodrafz@icloud.com>
2025-08-28 12:28:57 +0200

    input checking and asking to play again

--- game_logic.py
+++ game_logic.py
@@ -35,26 +35,42 @@ def play_game():
 
     while mistakes < max_mistakes:
         guess = input("Guess a letter: ").lower()
+        # not needed to check if type is str because the input always gives a string back
+        if type(guess) != str:
+            print("Invalid input. Please enter a single letter.")
 
-        if guess in guessed_letters:
-            print("You already guessed that letter.")
-            continue
+        if len(guess) != 1:
+            print("Invalid input. Please enter a single letter.")
 
-        if guess in secret_word:
-            guessed_letters.append(guess)
-            print("Excellent guess!")
         else:
-            mistakes += 1
-            print("Wrong guess!")
+            if guess in guessed_letters:
+                print("You already guessed that letter.")
+                continue
 
-        display_game_state(mistakes, secret_word, guessed_letters)
+            if guess in secret_word:
+                guessed_letters.append(guess)
+                print("Excellent guess!")
+            else:
+                mistakes += 1
+                print("Wrong guess!")
 
-        if all(letter in guessed_letters for letter in secret_word):
-            print("Congratulations! You guessed the word right!")
-            break
+            display_game_state(mistakes, secret_word, guessed_letters)
+
+            if all(letter in guessed_letters for letter in secret_word):
+                print("Congratulations! You guessed the word right!")
+                break
 
     else:
         print(f"Game over! The word was {secret_word}. Good luck next time!")
+
+    play_again = input("Do you want to play again? (Y/N): ").lower()
+    if play_again == "y":
+        play_game()
+    elif play_again == "n":
+        print("Thanks for playing!")
+    else:
+        print("Invalid input. Please enter 'Y' or 'N'.")
+
 
 
 if __name__ == "__main__":
