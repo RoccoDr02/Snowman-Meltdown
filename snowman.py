1657ef090335d7de085763955af7e7b9403d8683
RoccoDr02 <roccodrafz@icloud.com>
2025-08-28 12:37:38 +0200

    imported play_game into snowman delete everything else

--- snowman.py
+++ snowman.py
@@ -1,41 +1,5 @@
-import random
+from game_logic import play_game
 
-# List of secret words
-WORDS = ["python", "git", "github", "snowman", "meltdown"]
-
-
-def get_random_word():
-    """Selects a random word from the list."""
-    return WORDS[random.randint(0, len(WORDS) - 1)]
-
-
-def display_game_state(mistakes, secret_word, guessed_letters):
-    # Display the snowman for the current number of mistakes.
-    print(STAGES[mistakes])
-    # Build a display version of the secret word.
-    display_word = ""
-    for letter in secret_word:
-        if letter in guessed_letters:
-            display_word += letter + " "
-        else:
-            display_word += "_"
-    print("Secret word:", display_word)
-    print("\n")
-
-def play_game():
-    secret_word = get_random_word()
-    guessed_letters = []
-    mistakes = 0
-
-
-    print("Welcome to Snowman Meltdown!")
-    #For now, display the initial game state
-    display_game_state(mistakes, secret_word, guessed_letters)
-
-    # TODO: Build your game loop here.
-    # For now, simply prompt the user once:
-    guess = input("Guess a letter: ").lower()
-    print("You guessed:", guess)
 
 
 if __name__ == "__main__":
